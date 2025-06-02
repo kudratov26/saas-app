d907bfdee893699b480f3b7ce62c79cbf3237860
kudratov26 <qudratov.com26@gmail.com>
2025-06-02 15:59:26 +0500

    implement navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Link } from "lucide-react"
+import Image from "next/image"
+import NavItems from "./NavItems"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link>
+                <div className="flex items-center gap-2.5 cursor-pointer">
+                    <Image
+                        src={"/images/logo.svg"}
+                        alt="logo"
+                        width={46}
+                        height={44}
+                    />
+                </div>
+            </Link>
+            <div className="flex items-center gap-8">
+                <NavItems />
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
